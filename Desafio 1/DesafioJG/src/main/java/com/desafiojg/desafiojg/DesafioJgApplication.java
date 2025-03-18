4f67169af434dd4dbb0dcd16e89bbc5ead75521f
Darkmork <jorge.gangale@mail.udp.cl>
2025-03-17 21:12:50 -0300

    Creación proyecto

---
+++ Desafio 1/DesafioJG/src/main/java/com/desafiojg/desafiojg/DesafioJgApplication.java
@@ -0,0 +1,13 @@
+package com.desafiojg.desafiojg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioJgApplication.class, args);
+    }
+
+}
---
+++ Desafio 1/DesafioJG/src/test/java/com/desafiojg/desafiojg/DesafioJgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiojg.desafiojg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
