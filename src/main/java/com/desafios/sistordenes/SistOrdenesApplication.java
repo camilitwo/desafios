677f7b9fb32432c2c4e5ebec959352a7b9d9a556
Darkmork <jorge.gangale@mail.udp.cl>
2025-04-06 20:39:13 -0400

    Avance en la API desde rama jorge

---
+++ src/main/java/com/desafios/sistordenes/SistOrdenesApplication.java
@@ -0,0 +1,13 @@
+package com.desafios.sistordenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistOrdenesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistOrdenesApplication.class, args);
+    }
+
+}
