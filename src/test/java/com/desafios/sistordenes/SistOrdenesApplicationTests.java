4b2d53801f21cd9950c65b0ed7905985aae7796c
Darkmork <jorge.gangale@mail.udp.cl>
2025-04-12 20:31:41 -0400

    Programa terminado desafio semana 4 con test para que pase H2

--- src/test/java/com/desafios/sistordenes/SistOrdenesApplicationTests.java
+++ src/test/java/com/desafios/sistordenes/SistOrdenesApplicationTests.java
@@ -2,12 +2,13 @@ package com.desafios.sistordenes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 👉 Esto activa application-test.yml
 class SistOrdenesApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
